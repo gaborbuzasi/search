a44628059e41007d8eef3cca715f79881013a689
Giuseppe Broccolo <g.broccolo.7@gmail.com>
2019-02-16 12:36:18 +0100

    start integration of all the pipeline

---
+++ ocr/ocr/elastic.py
@@ -0,0 +1,82 @@
+from elasticsearch import Elasticsearch
+from operator import itemgetter
+
+class elastic(object):
+
+    def __init__(self):
+        self._es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
+        if not self._es.ping():
+            raise Exception("Cannot connect to ElasticSearch")
+
+    def create_index(self, index_name='elastic'):
+        created = False
+        # index settings
+        settings = {
+            "mappings": {
+                "members": {
+                    "dynamic": "strict",
+                    "properties": {
+                        "folder": {
+                            "type": "text"
+                        },
+                        "filename": {
+                            "type": "text"
+                        },
+                        "text": {
+                            "type": "text",
+                            "analyzer": "german",
+                            "fields": {
+                                "raw": {
+                                    "type":"keyword"
+                                }
+                            }
+                        },
+                    }
+                }
+            }
+        }
+        try:
+            if not self._es.indices.exists(index_name):
+                # Ignore 400 means to ignore "Index Already Exist" error.
+                self._es.create(index=index_name, ignore=400, body=settings, doc_type="_doc", id=1)
+            created = True
+        except Exception as ex:
+            print(str(ex))
+        finally:
+            return created
+
+    def delete_index(self, index_name):
+        self._es.indices.delete(index=index_name, ignore=[400, 404])
+
+    def store_record(self, index_name, doc_type, record):
+        is_stored = True
+        try:
+            item = self._es.get(index=index_name, doc_type='_doc', id=record['filename'], ignore=[404])
+            if item['found'] == True:
+                print('Item with id %s already stored' % (record['filename']))
+                return True
+            outcome = self._es.index(index=index_name, doc_type=doc_type, body=record, id=record['filename'])
+        except Exception as ex:
+            print(str(ex))
+            is_stored = False
+        finally:
+            return is_stored
+
+    def search(self, index_name, query_phrase):
+        query_object = {'query': {'match_phrase': {'text': query_phrase }}}
+        response = self._es.search(index=index_name, body=query_object)
+        return response
+
+    def get_best_result(self, response):
+        filenames = {}
+        for hit in response['hits']['hits']:
+            score = hit['_score']
+            filename = hit['_source']['filename']
+            if filename not in filenames:
+                filenames[filename] = score
+            else:
+                filenames[filename] += score
+        if len(filenames) > 0:
+            filenames_sorted_by_score = sorted(filenames.items(),key=itemgetter(1), reverse=True)
+            filename = filenames_sorted_by_score[0][0]
+            return filename
---
+++ ocr/ocr/load_data_into_json.py
@@ -0,0 +1,155 @@
+import pytesseract
+from pdf2image import convert_from_path
+import docx
+import xlrd
+import csv
+import os
+import json
+import re
+
+def load_stop_words():
+   stop_files = [
+       '/var/lib/ocr/stopwords-de.txt',
+       '/var/lib/ocr/stopwords-en.txt',
+       '/var/lib/ocr/stopwords-it.txt']
+
+   stop_words = []
+
+   for stopf in stop_files:
+       fh = open(stopf, "r")
+       stop_content = fh.read()
+       fh.close()
+       stop_words += re.split(r"/|\n| |:", stop_content)
+
+
+def reduce_file(input_text):
+   load_stop_words()
+
+   input_text = input_text.lower()
+   input_text = re.sub('\n', ' ', input_text)
+   all_words = re.split(
+       r"/|\?|#|=|&|\. |\.\n,|\||!| |:|, | ,|\)|\(|- -|- | -|;", input_text)
+
+   return ' '.join(
+           list(set(
+               [w for w in all_words
+                if w not in ['','-','--','>','+','*']
+                and w not in stop_words])))
+
+
+def clean(location, filename, txt_content):
+   content = {}
+   content['folder'] = location
+   content['filename'] = filename
+   content['text'] = reduce_file(txt_content)
+
+   json.dump(content, outfile)
+
+
+def pdf_to_txt(input_pdf):
+    pages = convert_from_path(input_pdf)
+    number = 0
+
+    txt = ''
+    for page in pages:
+        number = number + 1
+        txt = txt + pytesseract.image_to_string(page)
+
+    return txt
+
+
+def image_to_txt(input_image):
+    page = convert_from_path(input_image)
+    image_format = os.path.splitext(input_image)[-1].lower()
+
+    # Check if it a PDF or TXT or CSV or Excel or JPEG or JPG or PNG TIF or GIF
+    file_extension = ''
+    if image_format == '.png':
+        file_extension = 'PNG'
+    elif image_format == '.jpeg' or image_format == 'jpg':
+        file_extension = 'JPEG'
+    elif image_format == '.tif':
+        file_extension = 'TIF'
+    else:
+        print('The file extension are not supported')
+
+    if file_extension is not '':
+        txt = pytesseract.image_to_string(page)
+        return txt
+    else:
+        print('Error')
+
+
+def docx_to_text(input_docx):
+    doc = docx.Document(input_docx)
+
+    txt = ''
+
+    for para in doc.paragraphs:
+        txt += para.text.encode('ascii', 'ignore')
+
+    return txt
+
+
+def xls_to_txt(input_xls):
+    x = xlrd.open_workbook(input_xls)
+    # All table sheets ?
+    x1 = [x.sheet_names()]
+
+    input_csv = ''
+    txt = ''
+
+    for x2 in x1:
+        for i in range(x2.nrows):
+            input_csv += input_csv.writerow(x2.row_values(i))
+
+        [txt.write(" ".join(row) + '\n') for row in csv.reader(input_csv)]
+
+    return txt
+
+
+def csv_to_txt(input_csv):
+    txt = ''
+
+    with open(input_csv, "r") as my_input_file:
+        [txt.write(" ".join(row) + '\n') for row in csv.reader(my_input_file)]
+
+    return txt
+
+
+def find_files(root, ext):
+    for root, dirs, files in os.walk(root):
+        for f in files:
+            if f.lower().endswith(ext):
+                f = os.path.join(root, f)
+                yield f
+
+        for d in dirs:
+            d = os.path.join(root, d)
+            find_files(d, ext)
+
+
+def start_the_data_input_process(fp):
+    ext = os.path.splitext(fp)[-1].lower()
+
+    if ext == '.pdf':
+        output = pdf_to_txt(fp)
+        return reduce_file(output)
+    elif ext == '.docx':
+        output = docx_to_text(fp)
+        return reduce_file(output)
+    elif ext == '.csv':
+        output = csv_to_txt(fp)
+        return reduce_file(output)
+    elif ext == '.png' or ext == '.jpg' or ext == '.jpeg' or ext == '.tif':
+        output = image_to_txt(fp)
+        return reduce_file(output)
+    elif ext == '.xls' or ext == '.xlsx':
+        output = xls_to_txt(fp)
+        return reduce_file(output)
+    elif ext == '.db':
+        print("Db Files as Thumpnails not working!")
+        return None
+    else:
+        print("This extension not working!")
+        return None
